d6a41fc1b58e6b24c73daf2d1dca96533e609d3c
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-02-07 01:30:46 +0800

    Update env.py

--- env.py
+++ env.py
@@ -10,17 +10,10 @@ class graph_chase:
         self.num_node = num_node
         self.edge_prob = edge_prob
         
-        # build graph
-        while True:
-            self.G = build_graph(num_node, edge_prob)
-            number_components = nx.number_connected_components(self.G)
-            largest_components = max(nx.connected_components(self.G), key=len)
-            # 保证整个图是连通的
-            if len(largest_components) == self.num_node:
-                break
-        self.set_values()
-        
-        
+        self.G = None
+        self.current_location = None
+        self.previous_location = None
+        self.target_location = None
         # nx.degree(self.G)
         # DVweight = self.G.degree()
         # degree_sum = sum(span for n, span in DVweight) 		#各节点度数之和
@@ -33,9 +26,42 @@ class graph_chase:
         # print("连通子图个数: "+str(nx.number_connected_components(self.G)))
         # nx.draw_networkx(G, with_labels=True)
         # plt.show(
-    def step(self,action):
+    
+    def reset(self,):
+        # build graph
+        while True:
+            self.G = build_graph(self.num_node, self.edge_prob)
+            number_components = nx.number_connected_components(self.G)
+            largest_components = max(nx.connected_components(self.G), key=len)
+            # 保证整个图是连通的
+            if len(largest_components) == self.num_node:
+                break
+            
+        # 设置node和edge的数值
+        self.set_values()
+        self.set_edges()
+        
+        # 设置智能体和目标的位置
+        self.set_location()
+        self.previous_location = self.current_location
+        
+        
+        
+    def update_value(self,):
+        '''
+        更新节点数值
+        '''
+        
+    def step(self, action):
         pass
     
+    def set_location(self,):
+        node_list = list(self.G.nodes)
+        self.current_location = random.choice(node_list)
+        node_list.pop(self.current_location)
+        self.target_location = random.choice(node_list)
+        
+        
     def set_value(self, n, v):
         '''
         set the value for node n
@@ -50,10 +76,20 @@ class graph_chase:
         for node in self.G.nodes:
             value = np.random.rand()
             self.set_value(node, value)
-        print([self.G.nodes[n]['value'] for n in self.G.nodes])
+        # print([self.G.nodes[n]['value'] for n in self.G.nodes])
     
-    def set_edge(self, ):
-        print(self.G.edges[0][1])
+    def set_edge(self, e, v):
+        self.G.edges[e]['weight'] = v
+        
+    def set_edges(self,):
+        for e in list(self.G.edges):
+            value = np.random.rand()*2-1
+            self.set_edge(e, value)
+    
+    def get_neighbor(self, node):
+        nei = nx.neighbors(self.G, node)
+        # print(self.G[node])
+        return list(nei)
 
 def build_graph(num_node, prob):
     '''
@@ -80,4 +116,8 @@ def build_graph(num_node, prob):
 
 
 env = graph_chase(num_node=20, edge_prob=0.4)
-env.set_edge()
+# env.set_edges()
+# print(list(env.G.edges.data())[0])
+# env.get_neighbor(15)
+env.reset()
+# env.set_location()
